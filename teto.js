4c8757c40ab4b3927b886b164be532b4e2869676
Panda <pandav0x@hotmail.fr>
2021-01-25 14:31:42 +0100

    Add database connection

--- teto.js
+++ teto.js
@@ -1,21 +1,27 @@
 'use strict';
 
-console.log('Teto running.');
+console.log('Teto is waking up.');
 
 require('dotenv').config();
 
+const sqlite3 = require('sqlite3');
 const Discord = require('discord.js');
+
+//SQLite3 Database
+let db = new sqlite3.Database('./database/teto.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE);
+
+//Discord Client
 const client =  new Discord.Client();
 
 const commandHandler = require('./commands');
 
-console.log('Attempting connection.');
+console.log('Attempting connection to discord.');
 
 client.login(process.env.BOT_TOKEN);
 
 client.on('ready', () => {
     console.info('Connected.');
-    client.user.setActivity('Being worked on.');
+    client.user.setActivity('with the nerves of his creator.');
 });
 
 client.on('message', commandHandler);
