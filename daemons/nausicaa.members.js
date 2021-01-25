95e7c7a75f03c6c3486a4dc33011f8e90cae318a
Panda <pandav0x@hotmail.fr>
2021-01-25 14:00:34 +0100

    Create daemons folder (nothing more)

---
+++ daemons/nausicaa.members.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const fetch = require('node-fetch');
+
+module.exports = async (msg, args) => {
+
+    //let url = 'https://xivapi.com/freecompany/9235334723364991224?data=FCM';
+    //let response = await fetch(url);
+    //let json = await response.json();
+
+
+    //let members = json.FreeCompanyMembers;
+
+};
