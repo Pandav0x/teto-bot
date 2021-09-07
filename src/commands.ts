86892fab7786e2511ff232d582c3dd2a60b63309
Pandav0x <pandav0x@hotmail.fr>
2021-09-07 23:29:23 +0200

    Add exclusion for .map files

--- src/commands.ts
+++ src/commands.ts
@@ -2,6 +2,7 @@
 
 import { Message } from "discord.js";
 import * as fs from "fs";
+import * as path from "path";
 import Command from "./contracts/command";
 
 export default class MessageHandler {
@@ -9,27 +10,41 @@ export default class MessageHandler {
     commands: {[key: string]: Command};
 
     constructor() {
-        let a: any = this.registerCommands();
-        console.log(a);
+        let a: {[key: string]: Command} = this.registerCommands();
         this.commands = a;
     }
 
     registerCommands(): {[key: string]: Command} {
         let commands: {[key: string]: Command} = {};
-        fs.readdirSync('./src/commands').forEach(function(commandFile: string) {
-            let requiredCommand = require('./commands/' + commandFile);
+       
+        fs.readdirSync(`${__dirname}/commands`).forEach((commandFile: string) => {
+            
+            if(path.extname(commandFile) === ".map") {
+                return;
+            }
+
+            let requiredCommand = require(`${__dirname}/commands/${commandFile}`); 
+
+            if(!requiredCommand.hasOwnProperty("getName") || typeof requiredCommand.getName !== "function") {
+                return;
+            }          
+
             commands[requiredCommand.getName()] = requiredCommand;
             if(requiredCommand.getAliases() !== []){
                 requiredCommand.getAliases().forEach((alias: string) => {
                     commands[alias] = commands[requiredCommand.getName()];
                 });
             }
-        });
+        });        
+
         return commands;
     }
 
     handle(msg: Message): void {
-        console.info(`${msg.author.username}: ${msg.content}`);
+       
+       console.log('coucou');
+       
+        /*console.log(`${msg.author.username}: ${msg.content}`); 
 
         let tokens: Array<string> = msg.content.split(' ');
         let command: string|undefined = tokens.shift();
@@ -38,14 +53,17 @@ export default class MessageHandler {
             return;
         }
 
+        console.log(process.env.BOT_PREFIX);
+        
+
         if(command.charAt(0) === process.env.BOT_PREFIX){
-            command = command.substr(1);
+            command = command.substr(1); //FIXME
             if(this.commands.hasOwnProperty(command)){
                 let response: number = this.commands[command].execute(msg, tokens);
                 if(response === 0){
                     msg.channel.send(`> ${this.commands[command].getHelp()}`);
                 }
             }
-        }
+        }*/
     }
 }
--- src/teto.ts
+++ src/teto.ts
@@ -7,7 +7,8 @@ import { Client } from "discord.js";
 import MessageHandler from "./commands";
 
 dotenv.config();
-const client =  new Client();
+
+const client =  new Client({ intents: 7 });
 
 console.log('Attempting connection to discord.');
 
@@ -15,7 +16,7 @@ client.login(process.env.BOT_TOKEN);
 
 client.on('ready', () => {
     console.info('Connected.');
-    client.user?.setActivity('meh.');
+    client.user?.setActivity('haha.');
 });
 
 client.on('message', (new MessageHandler()).handle);
