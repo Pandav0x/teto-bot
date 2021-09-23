14b5cbbc8ab965d16d5c17839ed3833215d1b799
Pandav0x <pandav0x@hotmail.fr>
2021-09-23 09:18:09 +0200

    Tidying join command

--- src/commands/join.js
+++ src/commands/join.js
@@ -13,16 +13,10 @@ module.exports = {
         return `${process.env.BOT_PREFIX}join`;
     },
     execute: (msg, args) => {
-        let userVoiceChan = msg.guild.members.cache.get(msg.author.id).voice.channel;
-        const voiceChannel = msg.member.voice.channel
-
-        console.log(
-            msg,
-            voiceChannel
-        )
+        let userVC = msg.member.voice.channel;
         
-        voiceChannel.join().then(() => {
-            console.log(`Joined "${voiceChannel.name}" voice chat from "${voiceChannel.guild.name}" server.`);
+        userVC.join().then((connection) => {
+            console.log(`Joined "${userVC.name}" voice chat from "${userVC.guild.name}" server.`);
         }).catch((err) => {
             console.error(err.message);
         });
