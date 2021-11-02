e0e007e550931b5af7a8d5c7bd9ff7d787a18451
Pandav0x <pandav0x@hotmail.fr>
2021-11-02 23:41:22 +0100

    Add emoji enum + assets

---
+++ src/utils/Emoji.ts
@@ -0,0 +1,7 @@
+// Enum values are from the imported emojis from assets/emojis to discord
+
+export enum Emoji {
+    TANK = '<:tank:905216107873452034>',
+    HEALER = '<:healer:905216107365941270>',
+    DPS = '<:dps:905216107508551691>'
+}
