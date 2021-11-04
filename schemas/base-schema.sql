93ddc29e150429dc5cb66985febd2d495f1c93e1
Pandav0x <pandav0x@hotmail.fr>
2021-11-04 01:53:59 +0100

    Remove useless index.ts in command folder

--- schemas/base-schema.sql
+++ schemas/base-schema.sql
@@ -1,2 +1,3 @@
 CREATE TABLE IF NOT EXISTS general_info (key TEXT, value TEXT);
-CREATE TABLE IF NOT EXISTS raids (raid_id INTEGER, date TEXT, hour TEXT);
+CREATE TABLE IF NOT EXISTS raids (raid_id INTEGER, date DATETIME, raid_json TEXT, announce_channel_id TEXT);
+CREATE TABLE IF NOT EXISTS birthday (birthday_id INTEGER, date DATE, user_id TEXT, announce_chanel_id TEXT);
