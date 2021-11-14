4cc556bfe7257af684c59f1418f363b69cdb27b7
Pandav0x <pandav0x@hotmail.fr>
2021-11-14 11:41:40 +0100

    Remove some spaces

--- src/utils/DatabaseManager.ts
+++ src/utils/DatabaseManager.ts
@@ -52,8 +52,6 @@ export default class DatabaseManager {
                             }
                         });
                     }
-
-
                 }                
             }
         });
