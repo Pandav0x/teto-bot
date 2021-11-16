29c40cf5b10cd8d5820dbd95d1020efc27995972
Pandav0x <pandav0x@hotmail.fr>
2021-11-16 08:59:49 +0100

    Add default value for time in command schema

--- schemas/base-schema.sql
+++ schemas/base-schema.sql
@@ -1,2 +1,2 @@
 CREATE TABLE IF NOT EXISTS general_info (id INTEGER PRIMARY KEY NOT NULL, info_key TEXT, info_value TEXT);
-CREATE TABLE IF NOT EXISTS command (id INTEGER PRIMARY KEY NOT NULL, teto_message_id INTEGER, user_id INTEGER, user_message_id INTEGER, teto_message_content TEXT, time DATETIME);
+CREATE TABLE IF NOT EXISTS command (id INTEGER PRIMARY KEY NOT NULL, teto_message_id INTEGER, user_id INTEGER, user_message_id INTEGER, teto_message_content TEXT, time INTEGER NOT NULL DEFAULT (strftime('%s','now')));
