786293c97b7f97be37ec891e597fe1b4e0ba8915
Pandav0x <pandav0x@hotmail.fr>
2021-12-29 20:13:51 +0100

    Add alert handling life cycle

--- src/alert/TickHandler.ts
+++ src/alert/TickHandler.ts
@@ -1,5 +1,17 @@
 export default class TickHandler {
     handle(currentTime: Date){
-        console.log(`${currentTime.getHours()}:${currentTime.getMinutes()}:${currentTime.getSeconds()}`);
+        //console.log(`${currentTime.getHours()}:${currentTime.getMinutes()}:${currentTime.getSeconds()}`);
+
+        if(currentTime.getSeconds() == 0){
+            console.log('Fire event');
+        }
+
+        if(currentTime.getSeconds() == 20){
+            console.log('clean fired events');
+        }
+
+        if(currentTime.getSeconds() == 40){
+            console.log('load new events');
+        }
     }
 }
